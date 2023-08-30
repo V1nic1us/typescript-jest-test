1a864a7404119f04579d6ed717abf4af8203f0d1
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-08-29 23:14:16 -0400

    test: first test in jest

---
+++ src/services/persisty.spec.ts
@@ -0,0 +1,13 @@
+describe('Persisty 1', () => {
+  it('should return ten', () => {
+    const number = 10;
+    expect(number).toBe(10);
+  });
+});
+
+describe('Persisty 2', () => {
+  test('should return Marcus', () => {
+    const number = 'Marcus';
+    expect(number).toBe('Marcus');
+  });
+});
