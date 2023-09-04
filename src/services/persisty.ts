9d3b38baff41126e711c3b38a229a5fbc3137a1b
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-09-03 23:50:51 -0400

    feat: Added PersistyProtocol interface

--- src/services/persisty.ts
+++ src/services/persisty.ts
@@ -1,5 +1,7 @@
-export class Persisty {
-  saveOrder() {
+import { PersistyProtocol } from '../interfaces/persisty-protocol';
+
+export class Persisty implements PersistyProtocol {
+  saveOrder(): void {
     console.log('Pedido salvo com sucesso');
   }
 }
